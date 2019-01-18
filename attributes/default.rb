7cbe07ba89c0cc63ef334af8f402bdcb0b36fa8d
LG\dominic.wernham <dominic.wernham@barclaycard.co.uk>
2019-01-18 15:04:33 +0000

    Added the new URL and checksum for JDK 11

--- attributes/default.rb
+++ attributes/default.rb
@@ -155,8 +155,8 @@ default['java']['jdk']['10']['bin_cmds'] = %w(appletviewer jar javac javapackage
                                               wsimport)
 
 # x86_64
-default['java']['jdk']['11']['x86_64']['url'] = 'http://download.oracle.com/otn-pub/java/jdk/11.0.1+13/90cf5d8f270a4347a95050320eef3fb7/jdk-11.0.1_linux-x64_bin.tar.gz'
-default['java']['jdk']['11']['x86_64']['checksum'] = 'e7fd856bacad04b6dbf3606094b6a81fa9930d6dbb044bbd787be7ea93abc885'
+default['java']['jdk']['11']['x86_64']['url'] = 'https://download.oracle.com/otn-pub/java/jdk/11.0.2+7/f51449fcd52f4d52b93a989c5c56ed3c/jdk-11.0.2_linux-x64_bin.tar.gz'
+default['java']['jdk']['11']['x86_64']['checksum'] = '1f14572cae12adec64d28fdbf38935df7ced20b3efb54c33934b8bbecafdcd79'
 
 default['java']['jdk']['11']['bin_cmds'] = %w(jaotc jar jarsigner java javac javadoc javap jcmd jconsole jdb jdeprscan
                                               jdeps jhsdb jimage jinfo jjs jlink jmap jmod jps jrunscript jshell jstack
